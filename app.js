fa899ac1209d7bc7f082fbffa990ee519c349024
Diego Mota <diegomotalima417@gmail.com>
2025-06-27 21:42:44 -0300

    colocando o primeiro código

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 //testing commits in Git and GitHub 
 
 //testing second commit 
+
+//escolher um número aleatório entre 0 e 50
+
+let numeroAleatorio = Math.floor(Math.random() * 51); // de 0 a 50
+console.log("Número aleatório entre 0 e 50:", numeroAleatorio);
